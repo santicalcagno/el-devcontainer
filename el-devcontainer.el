697592b44db64b0b9276164edf7fc1f9ea535051
Santiago Calcagno <santicalcagno@gmail.com>
2020-05-31 22:46:48 -0300

    read dockerfile path from json

--- el-devcontainer.el
+++ el-devcontainer.el
@@ -1,7 +1,14 @@
+(require 'json)
+
 ;;;###autoload
-(defun open-in-container (devcontainer-json-path)
+(defun open-in-container ()
   "Opens the current project in a container for development"
   (interactive)
-  ("test")) 
+  (let* ((json-object-type 'hash-table)
+       (json-array-type 'list)
+       (json-key-type 'string)
+       (devcontainer-json (json-read-file "~/test.json"))
+       (dockerfile-path (gethash "dockerFile" devcontainer-json)))
+  (message dockerfile-path)))
 
 (provide 'el-devcontainer)
